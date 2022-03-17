daa9af75f4f3b08a0f8c645f7a6759845cd72bbe
Rafael Moura <rcmoura@gmail.com>
2022-03-17 20:39:40 -0300

    Create prg_inverso_absoluto.py

---
+++ prg_inverso_absoluto.py
@@ -0,0 +1,9 @@
+# programa inverso absoluto
+numero = float(input("Digite um numero: "))
+if numero > 0:
+    inverso = 1 / numero;
+    print ("Inverso: ", inverso)
+else:
+    absoluto = numero * -1
+    print ("Absoluto: ", absoluto)
+
