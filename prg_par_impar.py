7eebffcea64cbb63e121fa0c8c1515d080a6ea9a
Rafael Moura <rcmoura@gmail.com>
2022-03-17 21:29:50 -0300

    Create prg_par_impar.py

---
+++ prg_par_impar.py
@@ -0,0 +1,7 @@
+# programa par impar
+num = int(input("Entre com um numero: "))
+resto = num % 2
+if resto == 0:
+    print ("O numero, ",num," é PAR")
+else:
+    print ("O numero, ",num," é IMPAR")
