2cf1b55b93d2e8f4305cc98f1a0670d3d78d7f96
Rafael Moura <rcmoura@gmail.com>
2022-05-06 07:43:55 -0300

    Create prg_exemplo_for_1.py

---
+++ prg_exemplo_for_1.py
@@ -0,0 +1,10 @@
+# Estrutura de repetição for
+# 
+# for VARIAVEL in (FAIXA-DE-VALORES):
+#   INSTRUÇÃO
+#
+
+# OS VALORES PODEM SER LISTADOS EXPLICITAMENTE
+
+for x in (0,1,2,3,4,7,154,74):
+    print(x)
