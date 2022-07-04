ae5aa464fcd7618f19a2d10f398b94fd1f9fee76
Rafael Moura <rcmoura@gmail.com>
2022-07-04 13:04:29 -0300

    Create func1.py

---
+++ func1.py
@@ -0,0 +1,11 @@
+# definir função
+def maximo(a, b):
+    if a > b:
+        return a
+    else:    
+        return b
+
+# aplicando a função
+
+print(f"maximo(5,6) == 6 -> obtido:{maximo(5,6)}")
+print(f"maximo(2,1) == 2 -> obtido:{maximo(2,1)}")
