35b9c9fb6053e0936f76957a6bcf4553a853ff3b
Rafael Moura <rcmoura@gmail.com>
2022-07-04 13:04:32 -0300

    Create func2.py

---
+++ func2.py
@@ -0,0 +1,8 @@
+# definir função
+def multiplo(a,b):
+    return a%b == 0
+
+# aplicando a função
+
+print(f"Multiplo(8,4) == True -> obtido:{multiplo(8,4)}")
+print(f"Multiplo(7,3) == False -> obtido:{multiplo(7,3)}")
